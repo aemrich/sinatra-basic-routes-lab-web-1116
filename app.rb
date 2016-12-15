559878cacb5afbd71cbd56eae28993c5e86a12c6
aemrich <adam.emrich@gmail.com>
2016-12-15 12:00:09 -0500

    done

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    status 200
+    "My name is Adam."
+  end
+
+  get '/hometown' do
+    status 200
+    "My hometown is Briarcliff."
+  end
+
+  get '/favorite-song' do
+    status 200
+    "My favorite song is Moonlight Sonata"
+  end
+
 end
